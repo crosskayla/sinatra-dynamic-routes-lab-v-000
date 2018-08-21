f1ffe19e86fa2b39df9bc4a87373f8df3c1f275a
Kayla Cross <kac288@georgetown.edu>
2018-08-21 16:06:32 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -22,9 +22,9 @@ class App < Sinatra::Base
   end
 
   get '/say/:word1/:word2/:word3/:word4/:word5' do
-    @phrase = params[:phrase]
-    @num.times do
-      "#{@phrase} "
+    @words = [params[:word1], params[:word2], params[:word3], params[:word4], params[:word5]]
+    @words.each do |word|
+      "#{word} "
     end
   end
 
