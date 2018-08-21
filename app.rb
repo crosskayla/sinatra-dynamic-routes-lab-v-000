b0b886d15b0da1ff6e0e4e917391eff764376df7
Kayla Cross <kac288@georgetown.edu>
2018-08-21 16:17:43 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -31,7 +31,7 @@ class App < Sinatra::Base
   get '/:operation/:number1/:number2' do
 
     num1 = params[:number1].to_i
-    num2 = params[:number2].to_I
+    num2 = params[:number2].to_i
 
     case params[:operation]
     when 'add'
